d0990a1c164dc259ba3a46c2878e5c63bdc2ef97
ZackFra <zack11frazier22@gmail.com>
2021-07-29 18:05:49 -0400

    adding "add quest" functionality

--- Project1/force-app/main/default/staticresources/QuestPage.js
+++ Project1/force-app/main/default/staticresources/QuestPage.js
@@ -18,7 +18,7 @@ function insertBefore(id, HTMLString, index=0) {
 // @desc: hide a modal given its id
 // @id  : <string>
 function hideModal(event, id) {
-    event.preventDefault();
+    event?.preventDefault();
     document
     	.querySelector("#" + id)
     	.style
@@ -27,26 +27,92 @@ function hideModal(event, id) {
 
 // @desc: set a modal to visible given its id
 // @id  : <string>
-function showModal(event, id) {
-    event.preventDefault();
-    document.querySelector("#" + id)
-        .style
-        .display = 'initial';
+function showModal(event, id, message) {
+    event?.preventDefault();
+    const elem = document.querySelector("#" + id);
+    elem.style.display = 'initial';
+}
+    
+// @desc: because VF is weird, need a special function for this error modal
+function showErrorModal(event, id, message) {
+    event?.preventDefault();
+    const elem = document.querySelector("#" + id);
+    const content = elem.querySelector("#prompt-message-wrapper");
+    content.innerText = message;
+    elem.style.display='initial';
+}
+    
+// @desc: because VF is weird, need a special function for this error modal
+function hideErrorModal(event, id) {
+    event?.preventDefault();
+    const elem = document.querySelector("#" + id);
+    const content = elem.querySelector("#prompt-message-wrapper");
+    content.innerText = '';
+    elem.style.display='none';
 }
 
-
+// @desc : validate the parameters of the form data prior to submitting a new quest
+// @note : I hate to do it this way, but Apex makes it so the actionFunction component requires
+//       : a reRender component, meaning  I can't use hasError and errorMessage because the page
+//       : won't rerender as a whole. It's really awful.
+// @data : <object> the form data passed in 
+function validateParams(data) {
+    if(data['Required Profession: Adventurer'] === false && data['Required Profession: Craftsman'] === false) {
+    	return false;
+	}
+    
+    if(data['Quest Name'] === '') {
+    	return false;
+	}
+    
+    if(data['Location'] === '') {
+    	return false;
+	}
+    
+    if(data['Quest Details'].length < 10) {
+    	return false;
+	}
+    
+    const validDangerLevels = ['Certain Death', 'Hard', 'Medium', 'Easy', 'Beginner'];
+    if(!validDangerLevels.includes(data['Danger Level'])) {
+    	return false;
+	}
+    
+    if(data['Reward'] === NaN || data['Reward'] < 0) {
+    	return false;
+	}
+    
+    return true;
+}
+    
 // @desc       : retrieve the parameters, save a new quest
 // @event      : <event>
 // @echo       : <func> a function to pass parameters to the action function
 // @actionFunc : <func> the action function to save a quest
-function submitNewQuest(event, echo, actionFunc) {
-    event.preventDefault();
-    console.log(event);
+function submitNewQuest(event, actionFunc) {
+    event?.preventDefault();
     const data = Object.fromEntries(new FormData(event.target).entries());
     
     // set the checkbox variables
     ['Requires Party', 'Required Profession: Adventurer', 'Required Profession: Craftsman']
     .forEach(field => data[field] = (data[field] !== undefined));
-    // @todo: send this to an action function
-    console.log(data);
+    data['Reward'] = Number(data['Reward']);
+
+    if(!validateParams(data)) {
+    	showErrorModal(null, 'dynamic-error-modal', 'Unable to create quest, check your entered values.');
+    	hideModal(null, 'new-quest-modal');
+    	return;
+	}
+	
+    actionFunc(
+        data['Quest Name'],
+        data['Quest Details'],
+        data['Location'],
+        data['Requires Party'],
+        data['Danger Level'],
+        data['Required Profession: Adventurer'],
+        data['Required Profession: Craftsman'],
+        data['Reward']
+    );
+    hideModal(null, 'new-quest-modal');
 }
