d0bf2187ef0e75b21620280357b689660b635bfc
ZackFra <zack11frazier22@gmail.com>
2021-08-02 16:02:26 -0400

    added dynamic level javascript

---
+++ Project1/force-app/main/default/staticresources/UserPage.js
@@ -0,0 +1,15 @@
+// @desc: gather up all the levels on the page and sum them together
+function generateTotalLevel() {
+	let totalLevel = Array.from(document.querySelectorAll('#level-value'))?.map(node => Number(node.innerText)).reduce((value, acc) => acc + value, 0);
+	if(totalLevel === undefined) totalLevel = 0;
+    return totalLevel;
+}
+
+// @desc: onchange handler to reset the total level in the title     
+function tableChangeHandler() {
+    setTimeout(() => {
+    	document.querySelector('#level-table').querySelector('H3').innerText = 'Level: ' + String(generateTotalLevel());
+    }, 300);
+}
+    
+window.onload = tableChangeHandler;
